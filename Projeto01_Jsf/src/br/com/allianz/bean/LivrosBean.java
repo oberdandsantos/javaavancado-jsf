8d33f3154abaab87178b879cf708e39e9f98cab1
Oberdan dos Santos Evangelista <oberdan.evangelista@allianz.com.br>
2018-12-14 10:03:29 -0200

    Primeiro commit

---
+++ Projeto01_Jsf/src/br/com/allianz/bean/LivrosBean.java
@@ -0,0 +1,47 @@
+package br.com.allianz.bean;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.allianz.dao.LivrosDao;
+import br.com.allianz.models.Livro;
+
+@ManagedBean(name= "beanLivro")
+@RequestScoped
+public class LivrosBean {
+
+	private Livro livro;
+	public LivrosBean() {
+		if(livro == null) {
+			livro = new Livro();
+		}
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+
+		//metodo de ação (action) que, quando executado, retorna o
+	    //redirecionamento conforme o resultado da execução
+		public String incluirLivro() {
+			try {
+				new LivrosDao().IncluirLivro(livro);
+				return "sucesso";
+			} catch (Exception e) {
+				e.printStackTrace();
+				return "erro";
+			}
+		}
+	
+		//propriedade usada para apresentar a lista dos livros cadastrados
+		public List<Livro> getListaLivros() throws Exception {
+			return new LivrosDao().listarLivros();
+		}
+	
+}
---
+++ Projeto01_Jsf/src/br/com/allianz/models/Livro.java
@@ -0,0 +1,81 @@
+package br.com.allianz.models;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Livro {
+
+	private int id;
+	private String titulo;
+	private String autor;
+	private Date dataPublicacao;
+	private double preco;
+	
+
+	public Livro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Livro(int id, String titulo, String autor, Date dataPublicacao, double preco) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.autor = autor;
+		this.dataPublicacao = dataPublicacao;
+		this.preco = preco;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public Date getDataPublicacao() {
+		return dataPublicacao;
+	}
+
+	public void setDataPublicacao(Date dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+
+	//método para receber a data do formulario
+	public void setDataString(String dataString) {
+		try {
+			Date data = new SimpleDateFormat("dd/MM/yyyy").parse(dataString);
+			this.setDataPublicacao(data);
+		} catch (Exception e) {
+           this.setDataPublicacao(new Date());
+		}
+	}
+	
+	
+}
